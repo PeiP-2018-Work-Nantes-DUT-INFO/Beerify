0272ca0c8599e3273c47fb12fa1aff3dbb074f98
mxb <mxb@mxb.com>
2020-03-22 23:32:33 +0100

    Removed check handler

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Meta from 'vue-meta'
 import routes from '@/router/routes'
 import { store } from '@/store'
-import { checkIfTokenNeedsRefresh } from '@/utils/utils.js'
 import { checkForUpdates } from '@/utils/updates.js'
 import * as types from '@/store/mutation-types'
 
@@ -18,12 +17,6 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   checkForUpdates()
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  const isTokenSet = store.getters.isTokenSet
-  if (requiresAuth && !isTokenSet) {
-    return next('/login')
-  }
-  checkIfTokenNeedsRefresh()
   store.commit(types.SUCCESS, null)
   store.commit(types.ERROR, null)
   return next()
